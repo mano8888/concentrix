e76fbe9a6866c0cbaa4583868e162a8777aa011c
mano8888 <manukambakam.k@gmail.com>
2019-07-24 22:57:15 -0700

    XRevert "checking"
    This reverts commit 16d95438c8cbc2fecac4be159eca1eaa5de6e57a.

--- cont.sh
+++ cont.sh
@@ -1,11 +1,11 @@
-manu=$1
+manoj=$1
 
-echo "con containaers has been created"
+echo "creating $manoj containers"
 sleep 2;
 
-for i in `seq $manu`
+for i in `seq $manoj`
 do
-	echo "con containers www.com$i containers are created"
+	echo "creating www.com$i containers"
 	sudo docker run -it --name www.com$i mano8888/concentrix /bin/bash
 	sleep 1;
 	echo "www.com$i containers has been created"
--- hello.sh
+++
@@ -1,8 +0,0 @@
-for (( i=1; i<30; i++))
-do
-	if [ $i -eq 25 ] && [ $i -eq 28 ]
-	then
-		continue
-	fi
-	echo "$i"
-done
