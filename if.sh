59fabe04ddbe4f20ce24827a70a4b3ee7036e6c9
Ubuntu <ubuntu@ip-172-31-17-129.us-east-2.compute.internal>
2019-12-02 06:02:19 +0000

    commited a new file

---
+++ if.sh
@@ -0,0 +1,18 @@
+echo -e "entera filename: \c"
+
+read fname
+
+if [ -f $faname ]
+then
+	echo "if file exist write something & exist"
+                        
+	cat >> file
+
+	if [ -f $fname ]
+	then
+		echo "file exist"
+	else
+		echo "file not found"
+	fi
+echo "unknownfile"
+fi
